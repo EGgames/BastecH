89e47ca1ff949dd1aac7cb07177911d32d29d7ab
EGgames <emiliano.galmarini@gmail.com>
2014-07-30 14:06:04 -0300

    Important update
    Added
    -Stair Detection
    
    Changed name of "Bastech" to "Bastech_Arduino"

--- Bastech_Arduino/Bastech_Arduino.ino
+++ Bastech_Arduino/Bastech_Arduino.ino
@@ -1,7 +1,7 @@
 /*
------BASTECH rev 70------
+ -----BASTECH rev 75------
  -----Autor: Emiliano Galmarini------
- -----Bajo Licencia GPLv3-----------
+ -----Bajo Licencia GPLv3 -PROHIBIDO SU USO PARA CREAR PROYECTOS DE EGRESO DE EMT-UTU O SU USO EXCLUSIVO PARA CLUBES DE CIENCIA SIN LA AUTORIZACION DEL AUTOR-----------
  */
 #include <NewPing.h> //Libreria
 
@@ -27,6 +27,15 @@ int motor3 = 3; //Vibrador 3
 int speaker = 1; //Speaker 1
 int speaker_2= 0; //Speaker 2 (testeo)
 int input; //debug
+
+/* Deteccion de escaleras */
+int dato1=0; //Variable para alojar una medida
+int dato2=0; //Variable para alojar una medida
+int delta=0; //Variable para calcular abs
+/* Deteccion de escaleras */
+
+boolean flag=false; //Bandera
+boolean flag2=false; //Bandera
 
 void setup () {
   Serial.begin(115200); //Inia puerto serial a 9600 bauds
@@ -103,35 +112,35 @@ void srf05_arriba() {
     Serial.println("Objeto cerca (Sensor de arriba)"); //Imprime en el puerto que hay un objeto cerca del sensor de arriba
   }
 }
+
+/*----------DETECCION DE ESCALERAS-----------*/
 void srf05_escaleras() {
-  //delay(50); // Envia 2/pulsosseg. 30ms mas la distancia de tiempo mas corta entrwe pulso y pulso.
-  unsigned int uS3 = sonar3.ping(); //Envia ping2 entre microsecons.
-  Serial.print("Ping_3: "); //Imprime "Ping_2: " en puerto serial
-  Serial.print(sonar3.convert_cm(uS3)); //Convierte la distancia en resultado
-  Serial.println("cm"); //Imprime "cm" en puerto serial
-  int value3 = uS3; // Rango de lectura (0,1023)
+  //  delay(50);
+  unsigned int uS3 = sonar3.ping(); // Send ping, get ping time in microseconds (uS).
+  Serial.print("Ping3: ");
+  Serial.print(sonar3.convert_cm(uS3)); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+  Serial.println("cm");
+  unsigned int result=sonar3.convert_cm(delta); //Convierte a cm el resultado (Delta)
   int motor_speed3 = -(uS3/29); // PWM solo puede variar entre 255 valores
-  analogWrite(motor3,motor_speed3); //Modula el pulso segun la distancia pra que el vibrador gire mas o menos
+  analogWrite(motor2,motor_speed3); //Modula el pulso segun la distancia pra que el vibrador gire mas o menos
   Serial.println(motor_speed3); //Imprime velocidad del motor 2 en el puerto serial
-  if(motor_speed3>256 && motor_speed3<1){
-    delay(1);
-  }
-  if(motor_speed3>-70) { //Si el valor de giro del motor es mayor a -70
-    analogWrite(0,100); //Prende Speaker_2 a 100 de valor de 255
+  if(flag==false) { 
+    dato1=sonar3.ping(); //Guarda dato 1
+    flag=true; // Bandera en alto
   }
   else
-    if(motor_speed3<-70){ //Si es menor a -70
-      analogWrite(0,LOW); //Apaga el Speaker_2
-    }
-  if(motor_speed3<-256){ //En caso que este fuera del rango
-    analogWrite(motor3,LOW); //Apaga vibrador
-    delay(1); //Delay en 1 ms
+    (dato1=dato2); //Dato 1 es igual Dato 2
+  dato2=sonar3.ping();// Dato 2 pasa a ser otra medida
+  delta=abs(dato2-dato1); // Se calcula Valor absoluto de dato 2 - dato 1
+  Serial.println(sonar3.convert_cm(delta)); // Muestra resultado en Consola
+  Serial.println(result); //Debug
+  if(result >= 20) { 
+    digitalWrite(2, HIGH); 
   }
-  if(motor_speed3>-70 && motor_speed3<-1){
-    Serial.println("Objeto cerca (Escalera)"); //Imprime en el puerto que hay un objeto cerca del sensor de arriba
-  }
-  /*----------DETECCION DE ESCALERAS-----------*/
+  else
+    digitalWrite(2, LOW);
 }
+/*----------DETECCION DE ESCALERAS-----------*/
 
 void  loop() {
   delay(50); //50ms para los 2 sensores, asi no se retrasa el envio y se ahorra codigo
@@ -139,4 +148,3 @@ void  loop() {
   srf05_arriba(); //Inicializa Sensor de arriba
   srf05_escaleras(); //Inicializa Sensor de deteccion de escaleras
 }
-
